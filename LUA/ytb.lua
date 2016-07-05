566ae96b2c1e0d09e4ebefe32a3961b64789babc
Vitaly Denk <v.denk@upmtechnology.com>
2016-07-05 11:07:51 +0200

    add 4 uiCommand in test scenario

--- LUA/ytb.lua
+++ LUA/ytb.lua
@@ -50,9 +50,29 @@ local function DefineAndRegistervGATEdissector()
 	local oSubtree = oTreeItemRoot:add(oProtovGATE, oTvbData(), 'vGATE Protocol Data')
 	oSubtree:add(oTvbData(0 ,1), string.format('Version: %d', uiVersion))
 	local oSubtreeMessage = oSubtree:add (oTvbData(1), 'Message')
+	oSubtreeMessage:add(oTvbData(1, 1), string.format('Type: %d %s', uiType, sType))
+	oSubtreeMessage:add(oTvbData(2, 1), string.format('Command: %d %s', uiCommand, sCommand))
 	
+	if uiCommand == 2 and uiType == 1 then
+		oSubtreeMessage:add(oTvbData(3, 8), string.format('Date: %s', oTvbData(3, 8):string()))
+	end
 	
+	if uiCommand == 3 then
+		local uiLength = oTvbData(3, 1):uint()
+		oSubtreeMessage:add(oTvbData(3, 1), string.format('Length: %d', uiLength))
+		oSubtreeMessage:add(oTvbData(4, uiLength), string.format('Data: %s', oTvbData(4, uiLength):string()))
+	end
 	
+	if uiCommand == 4 then
+		if uiType == 0 then
+			local uiLength = oTvbData(3, 1):uint()
+			local sURL = oTvbData(4, uiLength):string()
+			oSubtreeMessage:add(oTvbData(3, 1), string.format('Length: %d', uiLength))
+			oSubtreeMessage:add(oTvbData(4, uiLength), string.format('URL: %s', sURL))
+		elseif uiType == 1 then
+			local uiResult = oTvbData(3, 1):uint()
+			oSubtreeMessage:add(oTvbData(3, 1), string.format('Result: %d', uiResult))
+	end
 	
 	
 	

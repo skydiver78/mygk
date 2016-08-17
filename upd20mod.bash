c2e8ed258103c70fa43b6d66eff39d6483a3577b
VIDE <v.denk@upmtechnology.com>
2016-08-17 13:21:21 +0200

    counter moved to the update module

--- upd20mod.bash
+++ upd20mod.bash
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-port_slct()
+#######################################################################################################################################
+######																  #####  		
+######	           This script is for EC20 module firmware update only! Please don't try to update the UC20 module.               #####
+######	                              Bevor update  please register the SIM on needed port		       			  #####
+######																  #####
+#######################################################################################################################################
+
+port_slct()	
 {
-	echo "Please enter the module/port number for update <port_nr: 0 - 15>:"
-	read port_nr
+	read -p "Please enter the module/port number for update <port_nr: 0|4|8|12>: " port_nr 
 	case $port_nr in
 		0|4|8|12)
 			;;
@@ -38,27 +44,66 @@ port_slct()
 
 load_mod()
 {
-	rmmod option
-	rmmod usb_wwan
-	rmmod umtsquec
-	insmod usb_wwan.ko
-	insmod option.ko
+	if [ -f ./usb_wwan.ko -a -f ./option.ko ] ; then
+ 	  rmmod option
+	  rmmod usb_wwan
+	  insmod usb_wwan.ko
+  	  insmod option.ko
+	elif [ -f /boot/usb_wwan.ko -a -f /boot/option.ko ] ; then
+ 	  rmmod option
+	  rmmod usb_wwan
+	  insmod /boot/usb_wwan.ko
+  	  insmod /boot/option.ko
+	else
+	  echo "usb_wwan.ko and/or option.ko not found"
+	  exit 1  
+	fi
 }		
 
 update()
 {
-	echo "Module $port_nr ($EP) will be updated"
-	echo "Current FW: $crnt_ver"
-	fold=`find -type d | cut -d/ -f2 | grep "20"`
-	echo "New FW from folder $fold"
-	qec20upg $EP $fold
+	count=0
+	echo 6 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/poff
+	echo 4711 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/force_mode
+	while :
+	do
+		sleep 5
+		Poff=`cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/poff`
+		echo -n "."
+
+		if [ "$Poff" == "4711" ] ; then
+			echo "Module $port_nr ($EP) will be updated"
+			echo "Current FW: $crnt_ver"
+			fold=`find -type d | cut -d/ -f2 | grep "20"`
+			echo "New FW from folder $fold"
+			sleep 5
+			qec20upg $EP $fold
+			echo 0 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/poff
+			echo 4711 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/force_mode
+			echo "Done"
+			count=0
+			return
+		else
+			count=`expr $count + 1`
+			if [ "$count" -gt 15 ] ; then
+				echo 0 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/poff
+				echo 4711 > /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/force_mode
+				echo "timeout"
+				return 0
+			fi	
+		fi
+	done
 }	
 
 upd_again()
 {
+	echo "OK, I´ll do it for you again!"
+
 	port_slct
-		crnt_ver=`cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version`
-		cur_fw=`expr $(cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version) : '\(.....\).*'`
+
+	crnt_ver=`cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version`
+	cur_fw=`expr $(cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version) : '\(.......\).*'`
+
 	if [ "$cur_fw" == "$new_fw" ] ; then	
 		update
 		else
@@ -73,16 +118,17 @@ else
 	echo "USB stick was recognized"
 	usbstk=`mount | grep "/dev/.*/ramdisk/usb/" | cut -d " " -f 3`
 	cd $usbstk
-		port_slct
-			new_fw=`find -type d | cut -d/ -f2 | grep "20" | cut -c 1-5`
-			crnt_ver=`cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version`
-			cur_fw=`expr $(cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version) : '\(.....\).*'`
-#	echo "crnt_ver=$crnt_ver"
-#	echo "cur_fw=$cur_fw"
-#	echo "new_fw=$new_fw"
+
+	port_slct
+
+	new_fw=`find -type d | cut -d/ -f2 | grep "20" | cut -c 1-7`
+	crnt_ver=`cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version`
+	cur_fw=`expr $(cat /proc/sys/vendor/teles/xgate/ctrl/c$port_nr/fw_Version) : '\(.......\).*'`
+
 	if [ "$cur_fw" = "$new_fw" ] ; then	
 		load_mod 
 		update
+#		/boot/xgact
 	else
 		echo "You try to update the module with wrong firmware!!! Please check the firmware on the usb stick and the current module version"
 		exit 2
@@ -91,13 +137,12 @@ fi
 
 while :
 do
-	echo "Would you like to update another module? (y/n)"
-	read answ
+	read -p "Would you like to update another module? (y/n) " answ
 	case $answ in
 	Y|y) 
-		upd_again "OK, lets do it again!";;
+		upd_again ;;
 	[Yy][Ee][Ss]) 
-		upd_again "OK, lets do it again!";;
+		upd_again ;;
 	N|n) exit ;;
 	[Nn][Oo]) exit ;;
 	*) echo "Invalid command"
